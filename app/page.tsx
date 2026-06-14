53ca7f635fba2fee89297af350e36d5fa0cbca2e
Justin Orr <justin.jorr1@gmail.com>
2026-06-14 11:26:07 -0500

    Update Header styling and navigation bar styling structure

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,27 @@
+import Image from "next/image";
+import Navbar from "@/components/Navbar";
+
 export default function HomePage() {
   return (
-    <div >
-      Home Page Placeholder
-    </div>
+    <header className="relative min-h-screen">
+      {/* Background */}
+      <Image
+        src="/images/home-page-hero-background.png"
+        alt="Minimalist dark mountain background image."
+        fill
+        priority
+        className="object-cover"
+      />
+
+      {/* Content */}
+      <section className="relative z-10">
+        <Navbar />
+        <section>
+          <h1>Justin Orr</h1>
+          <p>Full Stack Developer</p>
+          <button>Hire Me</button>
+        </section>
+      </section>
+    </header>  
   );
 }
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,15 +1,25 @@
+import Link from "next/link";
 import NavbarButton from "@/components/NavbarButton";
+
+const navContainerStyling = " mx-auto flex max-w-7xl items-center justify-between px-6 py-8";
+const logoLinkStyling = " text-xl font-semibold tracking-wide text-white";
+const navLinkStyling = "hidden md:flex gap-10";
 
 export default function Navbar() {
     return(
-        <nav>
-            <NavbarButton linkName="Home" urlPath="/"/>
-            <NavbarButton linkName="Skills" urlPath="/skills"/>
-            <NavbarButton linkName="Experience" urlPath="/experience"/>
-            <NavbarButton linkName="Services" urlPath="/services"/>
-            <NavbarButton linkName="Projects" urlPath="/projects"/>
-            <NavbarButton linkName="About" urlPath="/about"/>
-            <NavbarButton linkName="Contact" urlPath="/contact"/>
+        <nav className={navContainerStyling}>
+            <Link href="/" className={logoLinkStyling}>
+                JO
+            </Link>
+            <div className={navLinkStyling}>
+                <NavbarButton linkName="Home" urlPath="/"/>
+                <NavbarButton linkName="Skills" urlPath="/skills"/>
+                <NavbarButton linkName="Experience" urlPath="/experience"/>
+                <NavbarButton linkName="Services" urlPath="/services"/>
+                <NavbarButton linkName="Projects" urlPath="/projects"/>
+                <NavbarButton linkName="About" urlPath="/about"/>
+                <NavbarButton linkName="Contact" urlPath="/contact"/>
+            </div>
         </nav>
     );
 }
